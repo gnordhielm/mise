8db81c4e1f38f2a67e9c786f8fb51f368def2416
Gus Nordhielm <gnordhielm@gmail.com>
2017-04-08 19:09:36 -0700

    create React file structure

---
+++ index.js
@@ -0,0 +1,35 @@
+var express      = require('express'),
+    path         = require('path'),
+    favicon      = require('serve-favicon'),
+    morgan       = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser   = require('body-parser'),
+    session      = require('express-session'),
+    port         = process.env.PORT || 3000
+
+// load env variable from .env file
+require('dotenv').config()
+
+// create express app
+var app = express()
+
+// set up public directory path and favicon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(express.static('public'))
+
+
+// load general middleware
+app.use(morgan('dev'))
+app.use(cookieParser())
+app.use(bodyParser())
+
+// set up routes
+// var routes = require('./config/routes')
+// app.use('/api/', routes)
+
+// listen on port variable
+app.listen(port, function(){
+	var msg = `Server listening on port ${port}.`
+	var bracket = '='.repeat(msg.length+4)
+	console.log(`${bracket}\n| ${msg} |\n${bracket}`)
+})
